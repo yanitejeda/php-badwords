2de6bb76662738a765588e19ab09cda7f554ee0f
yani tejeda <83461543+yanitejeda@users.noreply.github.com>
2021-11-30 16:10:49 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -2,23 +2,26 @@
     Descrizione:
 *[Creare una variabile con un paragrafo di testo a vostra scelta].
 *[Stampare a schermo il paragrafo e la sua lunghezza.]
-*[Una parola da censurare viene passata dall’utente tramite parametro GET.]
+Una parola da censurare viene passata dall’utente tramite parametro GET.
 Stampare di nuovo il paragrafo e la sua lunghezza, 
 dopo aver sostituito con tre asterischi (***) tutte le occorrenze della parola da censurare.
  -->
 
 
 <?php 
- $paragrafo = "Testo del paragrafo";
+ $paragrafo = "cognome : tejeda";
 
 //elemento inserito dall'utente
  $cognomeUtente = $_GET['cognome'];
 
 //parola censurata sotituita
- $censura = str_replace($cognomeUtente," ","***");
- 
+ // $censura = str_replace($cognomeUtente," ","***" /* $paragrafo */);
+   $censura = str_replace($cognomeUtente, "***",$paragrafo); 
+
+ /* echo "<pre>";
+ var_dump($paragrafo);
+ echo "</pre>"; */
 ?>
 
-<h1><?php echo $paragrafo ?> N.(<?php echo  strlen($paragrafo) ?>) <?php echo $cognomeUtente ?></h1>
 
 <h1><?php echo $paragrafo ?> N.(<?php echo  strlen($paragrafo) ?>)  <?php echo $censura ?></h1>
